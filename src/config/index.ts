c2f5bfb71bd2835c6d6e475e1d9c9e119d22e16e
VishalPanchal <vishalpanchal570@gmail.com>
2024-05-06 19:30:54 +0530

    add test for persisit user

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 import path from "path";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-config({ path: path.join(__dirname, `../../.env/${process.env.NODE_ENV}`) });
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
 const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
     process.env;

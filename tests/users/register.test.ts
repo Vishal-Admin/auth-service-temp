41afa3280bc478cfd4e13bbe142ca1b7ca4eae58
VishalPanchal <vishalpanchal570@gmail.com>
2024-05-14 20:34:44 +0530

    test for .CUSTOMER role only

--- tests/users/register.test.ts
+++ tests/users/register.test.ts
@@ -2,8 +2,8 @@ import request from "supertest";
 import app from "../../src/app";
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../../src/config/data-source";
-import { truncateTables } from "../../tests/utils";
 import { User } from "../../src/entity/User";
+import { Roles } from "../../src/constants";
 
 describe("POST /auth/Register", () => {
     let connection: DataSource;
@@ -13,7 +13,8 @@ describe("POST /auth/Register", () => {
     });
 
     beforeEach(async () => {
-        await truncateTables(connection);
+        await connection.dropDatabase();
+        await connection.synchronize();
     });
 
     afterAll(async () => {
@@ -83,6 +84,24 @@ describe("POST /auth/Register", () => {
                 .post("/auth/register")
                 .send(userData);
             expect(user.body).toHaveProperty("id");
+        });
+
+        it("should assign only the customer role to the user", async () => {
+            const userData = {
+                firstName: "Vishal",
+                lastName: "Panchal",
+                email: "vishalpanchal570@gmail.com",
+                password: "secret",
+            };
+
+            await request(app).post("/auth/register").send(userData);
+            const userRepository = connection.getRepository(User);
+
+            const users = await userRepository.find();
+
+            expect(users[0]).toHaveProperty("role");
+
+            expect(users[0].role).toBe(Roles.CUSTOMER);
         });
     });
     describe("Filds are missing", () => {});

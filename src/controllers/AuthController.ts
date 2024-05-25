a0add0aabf72b0b023b17485895e1f5cdedd8c32
VishalPanchal <vishalpanchal570@gmail.com>
2024-05-25 08:09:24 +0530

    logout end point created

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -167,4 +167,24 @@ export class AuthController {
             return;
         }
     }
+
+    async logout(req: AuthRequest, res: Response, next: NextFunction) {
+        try {
+            await this.tokenService.deleteRefreshToken(Number(req.auth.id));
+            this.logger.info("Refresh token has been deleted", {
+                id: req.auth.id,
+            });
+            this.logger.info("User logged out successfully", {
+                id: req.auth.sub,
+            });
+
+            res.clearCookie("refreshToken");
+            res.clearCookie("accessToken");
+
+            res.json({});
+        } catch (error) {
+            next(error);
+            return;
+        }
+    }
 }

6e1ef42b52e361e6393e081fc94579694ef26605
VishalPanchal <vishalpanchal570@gmail.com>
2024-06-03 15:14:55 +0530

    setup new covrage for tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    varbose: true,
+    collectCoverage: true,
+    coverageProvider: "v8",
+    collectCoverageFrom: ["src/**/*.ts", "!tests/**", "!**/node_modules/**"],
 };
